d31b411286767d6f714b87a7f88fda75b1ebb616
Michael <michael.li@columbia.edu>
2020-10-10 14:05:23 -0400

    Putting testing and linting into django CI workflow

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,6 +1,11 @@
-def test_placeholder():
-    assert 1 == 1
+# Testing in Django https://docs.djangoproject.com/en/3.1/topics/testing/
+
+from django.test import TestCase
 
 
-def test_placeholder2():
-    assert 2 == 2
+class PlaceholderTestCase(TestCase):
+    def test_placeholder(self):
+        assert 1 == 1
+
+    def test_placeholder2(self):
+        assert 2 == 2
