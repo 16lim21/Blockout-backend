f4b4b9118255e0efe9a4f97396f799942ef7ac67
Michael <michael.li@columbia.edu>
2020-10-13 14:43:14 -0400

    fixing tests

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-console.log("hello world");
+// Creating simple REST API following this tutorial https://rahmanfadhil.com/express-rest-api/
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const messenger = require('./routers/messenger')
 
-// // Creating simple REST API following this tutorial https://rahmanfadhil.com/express-rest-api/
-// require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const messenger = require("./routers/messenger");
+const url = process.env.MONGODB_URI
 
-// const url = process.env.MONGODB_URI;
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) =>
+    console.log(`Error connecting to MongoDB: ${error.message}`)
+  )
 
-// mongoose
-//     .connect(url, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useFindAndModify: false,
-//     })
-//     .then(() => {
-//         console.log("Connected to MongoDB");
-//     })
-//     .catch((error) =>
-//         console.log(`Error connecting to MongoDB: ${error.message}`)
-//     );
+const app = express()
+app.use(express.json())
+app.use('/api', messenger)
 
-// const app = express();
-// app.use(express.json());
-// app.use("/api", messenger);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Started server on port ${PORT}`)
+})
 
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//     console.log(`Started server on port ${PORT}`);
-// });
+module.exports = app
--- test/test_placeholder.js
+++ test/test_placeholder.js
@@ -1,33 +1,7 @@
-var assert = require("assert");
-require("../index.js");
-describe("placeholder", function () {
-    it("shold return true", function () {
-        assert.equal(1, 1);
-    });
-});
-
-// let mongoose = require("mongoose");
-// let Book = require('../models/user');
-
-// //Require the dev-dependencies
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
-// let server = require('../index');
-// let should = chai.should();
-
-// chai.use(chaiHttp);
-// //Our parent block
-// describe('Books', () => {
-//   describe('/GET book', () => {
-//       it('it should GET all the books', (done) => {
-//         chai.request(server)
-//             .get('/book')
-//             .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('array');
-//               done();
-//             });
-//       });
-//   });
-
-// });
+var assert = require('assert')
+require('../index.js')
+describe('placeholder', function () {
+  it('shold return true', function () {
+    assert.equal(1, 1)
+  })
+})
