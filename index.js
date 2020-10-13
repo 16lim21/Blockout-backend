f3bb7895403cf5c3eecfdb303e522e41d1e8145b
Michael <michael.li@columbia.edu>
2020-10-13 16:38:41 -0400

    Attempting to use github secrets as environment variables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Creating simple REST API following this tutorial https://rahmanfadhil.com/express-rest-api/
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const mongoose = require('mongoose')
 const messenger = require('./routers/messenger')
