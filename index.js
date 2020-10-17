6c87eb70b25c29a865a18ae1d665c1bcb82244aa
Michael <michael.li@columbia.edu>
2020-10-16 23:03:08 -0400

    Adding code to interact with messenger

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-// Creating simple REST API following this tutorial https://rahmanfadhil.com/express-rest-api/
+// Using dotenv for development environment variables
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+    require('dotenv').config()
 }
-const express = require('express')
-const mongoose = require('mongoose')
-const messenger = require('./routers/messenger')
 
+const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) =>
-    console.log(`Error connecting to MongoDB: ${error.message}`)
-  )
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) =>
+        console.log(`Error connecting to MongoDB: ${error.message}`)
+    )
+
+const express = require('express')
+const users = require('./routers/users')
 
 const app = express()
+const http = require('http').createServer(app)
+
 app.use(express.json())
-app.use('/api', messenger)
+app.use('/api', users)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Started server on port ${PORT}`)
+http.listen(PORT, () => {
+    console.log(`Started server on port ${PORT}`)
 })
-
-module.exports = app
--- routers/messenger.js
+++ routers/messenger.js
@@ -1,57 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const User = require('../models/user')
-const router = express.Router()
+// API to handle communication between facebook messenger and Woof
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
-// Get all users
-router.get('/users', (req, res) => {
-  User.find({}).then((user) => res.json(user))
-})
+const login = require('facebook-chat-api')
 
-// Get individual user
-router.get('/users/:id', (req, res) => {
-  User.findById({ _id: mongoose.Types.ObjectId(req.params.id) })
-    .then((user) => {
-      if (user) {
-        res.json(user)
-      } else {
-        res.status(404).send({ error: 'user ID does not exist' }).end()
-      }
-    })
-    .catch((error) => res.send(error))
-})
+login(
+    {
+        email: `${process.env.MESSENGER_USER}`,
+        password: `${process.env.MESSENGER_PASS}`
+    },
+    (err, api) => {
+        if (err) return console.log(err)
 
-// Post new user
-router.post('/users', (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  })
+        api.listenMqtt((err, message) => {
+            if (err) return console.log(err)
 
-  user.save()
-    .then((result) => res.json(result))
-    .catch((error) => res.send(error))
-})
-
-// Update user
-router.patch('/users/:id', (req, res) => {
-  User.findByIdAndUpdate(
-    { _id: mongoose.Types.ObjectId(req.params.id) },
-    req.body,
-    { new: true }
-  )
-    .then((newUser) => {
-      res.json(newUser)
-    })
-    .catch((error) => res.send(error))
-})
-
-// Delete user
-router.delete('/users/:id', (req, res) => {
-  User.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) })
-    .then(() => {
-      res.status(204).end()
-    })
-    .catch((error) => res.send(error))
-})
-module.exports = router
+            if (message) {
+                api.sendMessage(message.body, message.threadID)
+            }
+        })
+    }
+)
