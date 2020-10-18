ba6e3bb5a6914f414cf891466ba6fa2553f25150
Michael <michael.li@columbia.edu>
2020-10-17 20:57:29 -0400

    Added babel to allow mocha to test on react

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -1,3 +1,4 @@
+// Some help with react testing https://www.pluralsight.com/guides/unit-test-react-component-mocha
 import React from "react";
 import { render } from "react-dom";
 import App from "../src/App";
