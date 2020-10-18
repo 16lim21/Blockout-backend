93a29891e8742da5924a9a9cce5e0016a5229fca
Michael <michael.li@columbia.edu>
2020-10-17 20:57:01 -0400

    Added babel to allow mocha to test on react

--- server/test/ignore-utils.js
+++ server/test/ignore-utils.js
@@ -2,10 +2,8 @@
 // Help from here: https://stackoverflow.com/questions/37259523/require-import-svg-during-mocha-tests
 import requireHacker from 'require-hacker'
 
-// const extensions = ['.svg', '.css', '.png', '.ico', '.txt']
 const extensions = ['svg', 'css', 'png', 'ico', 'txt']
 
 extensions.forEach((extension) => {
     requireHacker.hook(extension, (path) => '')
-    // require.extensions[extension] = () => ''
 })
