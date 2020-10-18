5015bf5efe07cfcd5dab0f52222a7674e9b013a4
Michael <michael.li@columbia.edu>
2020-10-17 21:21:46 -0400

    Reorganizing test directories and updating github workflow

--- test/client/App.test.js
+++ test/client/App.test.js
@@ -1,7 +1,7 @@
 // Some help with react testing https://www.pluralsight.com/guides/unit-test-react-component-mocha
 import React from "react";
 import { render } from "react-dom";
-import App from "../src/App";
+import App from "../../client/src/App";
 
 describe("renders app", () => {
     it("renders without crashing", () => {
