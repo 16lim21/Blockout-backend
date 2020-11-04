ffc56514a30142d1325bfd81d27946c69a999b15
Michael <michael.li@columbia.edu>
2020-11-03 21:05:58 -0500

    Server receives id token and sends back trivial response

--- server/routers/blockout.js
+++ server/routers/blockout.js
@@ -1,1 +1,15 @@
 // Modify google calendar using gcal api
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+// Receiving ID token
+router.post("/tokensignin", (request, response) => {
+    console.log(request.body);
+    response.set({
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+    });
+    response.send("received");
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,44 @@
 // Using dotenv for development environment variables
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
+// Connect to mongodb database
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
 
 mongoose
     .connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-        useCreateIndex: true
+        useCreateIndex: true,
     })
     .then(() => {
-        console.log('Connected to MongoDB')
+        console.log("Connected to MongoDB");
     })
     .catch((error) =>
         console.log(`Error connecting to MongoDB: ${error.message}`)
-    )
+    );
 
-const express = require('express')
-const users = require('./routers/users')
+// Setup express app
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
 
-const app = express()
-const http = require('http').createServer(app)
+app.use(express.json());
 
-app.use(express.json())
-app.use('/api', users)
+// Define application routing
+const users = require("./routers/users");
+app.use("/api", users);
 
-const PORT = process.env.PORT || 3001
+const blockout = require("./routers/blockout");
+app.use("/api", blockout);
+
+// Listen on specified port for express app
+const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
-    console.log(`Started server on port ${PORT}`)
-})
+    console.log(`Started server on port ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
