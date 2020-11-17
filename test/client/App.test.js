909257fbd90a899aafb1940116a2c36c9ccd7dc9
Michael <michael.li@columbia.edu>
2020-11-17 13:00:41 -0500

    Updating client test (haven't tested all new components yet)

--- test/client/App.test.js
+++ test/client/App.test.js
@@ -1,10 +1,10 @@
 // Some help with react testing https://www.pluralsight.com/guides/unit-test-react-component-mocha
 import React from 'react'
 import { render } from 'react-dom'
-import Login from '../../client/src/Login'
+import Login from '../../client/src/pages/Login'
 
 describe('Renders Login Page', () => {
-    it('Client Frontend should render without crashing', () => {
+    it('Login page should render without crashing', () => {
         const div = document.createElement('div')
         render(<Login />, div)
     })
