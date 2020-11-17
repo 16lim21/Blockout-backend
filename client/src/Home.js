f3f526069a96ced6ce91fffe147db08878bb2155
Michael <michael.li@columbia.edu>
2020-11-16 22:10:19 -0500

    adding home and router files

---
+++ client/src/Home.js
@@ -0,0 +1,14 @@
+/**
+ * Home page that users see once they have logged in
+ * @exports Home
+ */
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client/src/Router.js
@@ -0,0 +1,14 @@
+/**
+ * The router that switches page content between components
+ * @exports Router
+ */
+import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
+
+const Router = () => (
+    <Switch>
+        <Route exact path="/" component={Login}></Route>
+    </Switch>
+);
+
+export default Router;
