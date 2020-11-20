5123e1efcc661b296c53b9ea49f69e118862e8ba
Michael <michael.li@columbia.edu>
2020-11-20 00:12:25 -0500

    Changing user service export which could fix it

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,43 +1,47 @@
 const User = require('../models/user')
 
-class UserService {
-    /**
-     * Gets all users from mongodb database
-     */
-    findAllUsers () {
-        return User.find({})
-    }
-
-    /**
-     * Get user from mongodb database based on user id
-     * @param {string} userid - userid string from google login
-     */
-    getUser (userid) {
-        return User.findById({ _id: userid })
-    }
-
-    /**
-     * Post new user to mongodb
-     * @function
-     * @param {Object} userBody - object representing user account
-     * @returns {Promise} – Promise document representing the user document in mongoDB database
-     */
-    postUser (userBody) {
-        const user = new User(userBody)
-        return user.save()
-    }
-
-    patchUser (userid, body, flags) {
-        return User.findByIdAndUpdate({ _id: userid }, body, flags)
-    }
-
-    /**
-     * Delete specific user from mongoDB
-     * @param {string} userid - userId string from google login
-     */
-    deleteUser (userid) {
-        return User.deleteOne({ _id: userid })
-    }
+/**
+ * Gets all users from mongodb database
+ */
+function findAllUsers () {
+    return User.find({})
 }
 
-module.exports = new UserService()
+/**
+ * Get user from mongodb database based on user id
+ * @param {string} userid - userid string from google login
+ */
+function getUser (userid) {
+    return User.findById({ _id: userid })
+}
+
+/**
+ * Post new user to mongodb
+ * @function
+ * @param {Object} userBody - object representing user account
+ * @returns {Promise} – Promise document representing the user document in mongoDB database
+ */
+function postUser (userBody) {
+    const user = new User(userBody)
+    return user.save()
+}
+
+function patchUser (userid, body, flags) {
+    return User.findByIdAndUpdate({ _id: userid }, body, flags)
+}
+
+/**
+ * Delete specific user from mongoDB
+ * @param {string} userid - userId string from google login
+ */
+function deleteUser (userid) {
+    return User.deleteOne({ _id: userid })
+}
+
+module.exports = {
+    findAllUsers,
+    getUser,
+    postUser,
+    patchUser,
+    deleteUser
+}
