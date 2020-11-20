29fb94e0c37ef31fc14e1c5dee7879fc6c7effba
Michael <michael.li@columbia.edu>
2020-11-19 22:05:47 -0500

    Fixing some imports and npm scripts

--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,7 @@
 // Followed tutorial from https://www.digitalocean.com/community/tutorials/test-a-node-restful-api-with-mocha-and-chai#a-better-test
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const server = require('../../server/server')
+const server = require('../server/server')
 chai.should()
 chai.use(chaiHttp)
 const testId = '123456'
